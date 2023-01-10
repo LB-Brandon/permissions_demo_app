0b5b3cdffb0a4f03b72e18e2d03aacede3a69948
LB-Brandon <rmsdud623@gmail.com>
2023-01-10 20:54:55 +0900

    Feat 여러개의 하드웨어 권한 요청

--- app/src/main/java/com/brandon/permissionsdemo/MainActivity.kt
+++ app/src/main/java/com/brandon/permissionsdemo/MainActivity.kt
@@ -21,10 +21,50 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-    private fun showRationaleDialog(title: String, message: String, ) {
-        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
-        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel"){
-            dialog, _ -> dialog.dismiss()
+    private val cameraAndLocationResultLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted) {
+                    when (permissionName) {
+                        Manifest.permission.ACCESS_FINE_LOCATION -> {
+                            Toast.makeText(this, "Permission granted for location", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        Manifest.permission.ACCESS_COARSE_LOCATION -> {
+                            Toast.makeText(this, "Permission granted for coarse location", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        else -> {
+                            Toast.makeText(this, "Permission granted for Camera", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                } else {
+                    when (permissionName) {
+                        Manifest.permission.ACCESS_FINE_LOCATION -> {
+                            Toast.makeText(this, "Permission denied for fine location", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        Manifest.permission.ACCESS_COARSE_LOCATION -> {
+                            Toast.makeText(this, "Permission denied for coarse location", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        else -> {
+                            Toast.makeText(this, "Permission denied for Camera", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
+            }
+        }
+
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
         }
     }
 
@@ -33,13 +73,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnCameraPermission : Button = findViewById(R.id.btnCameraPermision)
+        val btnCameraPermission: Button = findViewById(R.id.btnCameraPermision)
 
-        btnCameraPermission.setOnClickListener{
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
-                showRationaleDialog("Permission Demo requires camera access", "Camera cannot be used because Camera access is denied")
-            }else{
-                cameraResultLauncher.launch(Manifest.permission.CAMERA)
+        btnCameraPermission.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && shouldShowRequestPermissionRationale(Manifest.permission.CAMERA))
+            {
+                showRationaleDialog(
+                    "Permission Demo requires camera access",
+                    "Camera cannot be used because Camera access is denied"
+                )
+            } else {
+                cameraAndLocationResultLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                )
             }
         }
 
