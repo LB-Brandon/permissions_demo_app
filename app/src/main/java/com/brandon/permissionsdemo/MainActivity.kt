368bd81bd5c978a77a1644c3be93cf95b68dcde9
LB-Brandon <rmsdud623@gmail.com>
2023-01-10 19:54:50 +0900

    Feat 하나의 하드웨어 권한 요청

--- app/src/main/java/com/brandon/permissionsdemo/MainActivity.kt
+++ app/src/main/java/com/brandon/permissionsdemo/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.brandon.permissionsdemo
 
+import android.Manifest
+import android.app.AlertDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    private val cameraResultLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission granted for camera", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission denied for camera", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun showRationaleDialog(title: String, message: String, ) {
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel"){
+            dialog, _ -> dialog.dismiss()
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCameraPermission : Button = findViewById(R.id.btnCameraPermision)
+
+        btnCameraPermission.setOnClickListener{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                showRationaleDialog("Permission Demo requires camera access", "Camera cannot be used because Camera access is denied")
+            }else{
+                cameraResultLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+
+
     }
 }
